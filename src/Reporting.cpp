d5b0a9c1722c2a0a5818908e7f759817be80cb7b
Ando <a.r.north23@gmail.com>
2023-02-03 20:13:01 +0000

    1) missing from previous commit

--- src/Reporting.cpp
+++ src/Reporting.cpp
@@ -1,5 +1,7 @@
 #include "Reporting.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 void Reporting::showReport(const std::vector<double> &timings)
 {
@@ -7,5 +9,30 @@ void Reporting::showReport(const std::vector<double> &timings)
     for (int i = 0; i < timings.size(); i++)
     {
         std::cout << "Test " << i + 1 << ": " << timings[i] << "ms" << std::endl;
+    }
+}
+
+void Reporting::printResults(const std::vector<double> &timings)
+{
+    for (int i = 0; i < timings.size(); i++)
+    {
+        std::cout << timings[i] << std::endl;
+    }
+}
+
+void Reporting::saveResults(const std::vector<double> &timings, const std::string &filePath)
+{
+    std::ofstream outputFile(filePath);
+    if (outputFile.is_open())
+    {
+        for (int i = 0; i < timings.size(); i++)
+        {
+            outputFile << timings[i] << std::endl;
+        }
+        outputFile.close();
+    }
+    else
+    {
+        std::cerr << "Unable to open file " << filePath << " for writing" << std::endl;
     }
 }
